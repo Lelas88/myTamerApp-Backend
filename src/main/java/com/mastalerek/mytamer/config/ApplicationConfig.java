1a65c16f4ae1d9a663eef76515c5a5bfe347294e
rmastalerek <robert.mastalerek@gmail.com>
2015-12-27 12:46:04 +0100

    HibernateTransactionManager changed to JPATransactionManager

--- src/main/java/com/mastalerek/mytamer/config/ApplicationConfig.java
+++ src/main/java/com/mastalerek/mytamer/config/ApplicationConfig.java
@@ -2,16 +2,22 @@ package com.mastalerek.mytamer.config;
 
 import java.util.Properties;
 
-import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
-import org.springframework.orm.hibernate4.HibernateTransactionManager;
-import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.google.common.base.Preconditions;
@@ -26,43 +32,42 @@ public class ApplicationConfig {
 	private Environment environment;
 
 	@Bean
-	public static PropertySourcesPlaceholderConfigurer propertyPlaceholderConfigurer() {
-		return new PropertySourcesPlaceholderConfigurer();
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+		em.setDataSource(dataSource());
+		em.setPackagesToScan(new String[] { "com.mastalerek.mytamer" });
+
+		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		em.setJpaVendorAdapter(vendorAdapter);
+		em.setJpaProperties(additionalProperties());
+
+		return em;
 	}
 
 	@Bean
-	public LocalSessionFactoryBean sessionFactory() {
-		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-		sessionFactory.setDataSource(restDataSource());
-		sessionFactory.setPackagesToScan(new String[] { "com.mastalerek.mytamer" });
-		// sessionFactory.
-		sessionFactory.setHibernateProperties(hibernateProperties());
-
-		return sessionFactory;
-	}
-
-	@Bean
-	public BasicDataSource restDataSource() {
-		BasicDataSource dataSource = new BasicDataSource();
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName(Preconditions.checkNotNull(environment.getProperty("jdbc.driverClassName")));
-		System.out.println("jdbc.password: ");
-		System.out.println(environment.getProperty("jdbc.password"));
 		dataSource.setUrl(Preconditions.checkNotNull(environment.getProperty("jdbc.url")));
 		dataSource.setUsername(Preconditions.checkNotNull(environment.getProperty("jdbc.username")));
 		dataSource.setPassword(Preconditions.checkNotNull(environment.getProperty("jdbc.password")));
-
 		return dataSource;
 	}
 
 	@Bean
-	public HibernateTransactionManager transactionManager() {
-		HibernateTransactionManager txManager = new HibernateTransactionManager();
-		txManager.setSessionFactory(sessionFactory().getObject());
+	public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(emf);
 
-		return txManager;
+		return transactionManager;
 	}
 
-	Properties hibernateProperties() {
+	@Bean
+	public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+		return new PersistenceExceptionTranslationPostProcessor();
+	}
+
+	Properties additionalProperties() {
 		return new Properties() {
 			/**
 			 * 
